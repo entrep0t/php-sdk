86c527d586f884e28688dbbf9c68f5920d7dd665
Ugo Stephant <ugo@poool.fr>
2020-05-01 11:33:52 +0200

    feat: additional cart methods

--- src/Cart.php
+++ src/Cart.php
@@ -120,4 +120,94 @@ class Cart
 
         return $result['cart'];
     }
+
+    /**
+     * @param string $couponName The coupon you want to apply to the current cart
+     * @param array[mixed] $options (optional) Guzzle request options
+     * @return array[mixed] Returns the current cart (or a new one)
+     *
+     * @example
+     * <code>
+     * $cart->applyCoupon('coupon-1');
+     * </code>
+     */
+    public function applyCoupon($couponName, $options = [])
+    {
+        $result = $this->client->request(array_merge($options, [
+            'method' => 'POST',
+            'url' => $this->client->getConfig('apiUrl').'/store/cart/coupon',
+            'json' => [
+                'coupon' => $couponName
+            ]
+        ]));
+
+        return $result['cart'];
+    }
+
+    /**
+     * @param string $address The address you want to add to the current cart
+     *      $address = [
+     *          'firstName' => string (required),
+     *          'lastName' => string (required),
+     *          'address' => string (required),
+     *          'address2' => string,
+     *          'city' => string (required),
+     *          'zipCode' => string (required),
+     *          'country' => string (required),
+     *          'state' => string,
+     *          'company' => string,
+     *          'phone' => string,
+     *          'email' => string,
+     *      ]
+     * @param array[mixed] $options (optional) Guzzle request options
+     * @return array[mixed] Returns the current cart (or a new one)
+     *
+     * @example
+     * <code>
+     * $cart->setShippingAddress([
+     *      'firstName' => 'John',
+     *      'lastName' => 'Doe',
+     *      'address' => '1 Infinite Loop',
+     *      'city' => 'Cupertino',
+     *      'zipCode' => '94014',
+     *      'country' => 'US',
+     * ]);
+     * </code>
+     */
+    public function setShippingAddress($address, $options = [])
+    {
+        $result = $this->client->request(array_merge($options, [
+            'method' => 'POST',
+            'url' => $this->client->getConfig('apiUrl').'/store/cart/address',
+            'json' => [
+                'address' => $address
+            ]
+        ]));
+
+        return $result['cart'];
+    }
+
+    /**
+     * @param string $methodId ID of the shipping method you want to apply to the current cart
+     * @param string $region Shipping region to calculate shipping fee & total cart amount
+     * @return array[mixed] Returns the current cart (or a new one)
+     *
+     * @example
+     * <code>
+     * $cart->setShippingMethod('shipping-1', 'world');
+     * </code>
+     */
+    public function setShippingMethod($methodId, $region = null, $options = [])
+    {
+        $result = $this->client->request(array_merge($options, [
+            'method' => 'POST',
+            'url' => $this->client->getConfig('apiUrl').'/store/cart/address',
+            'json' => [
+                'method' => $methodId,
+                'region' => $region
+            ]
+        ]));
+
+        return $result['cart'];
+    }
 }
